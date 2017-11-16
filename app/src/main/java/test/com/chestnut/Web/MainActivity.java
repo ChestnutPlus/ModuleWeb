dee7e2c8701a2011a1306529c6b9c7d2253b5530
Chestnut <974920378@qq.com>
2017-11-16 16:27:00 +0800

    ~

--- app/src/main/java/test/com/chestnut/Web/MainActivity.java
+++ app/src/main/java/test/com/chestnut/Web/MainActivity.java
@@ -3,7 +3,10 @@ package test.com.chestnut.Web;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.chestnut.Web.WebActivity;
 
@@ -16,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this,WebActivity.class);
-                intent.putExtra(WebActivity.URL,"https://www.baidu.com/");
-                startActivity(intent);
+                String s = ((EditText)findViewById(R.id.edit_url)).getText().toString();
+//                intent.putExtra(WebActivity.URL,"https://www.baidu.com/");
+                if (!TextUtils.isEmpty(s)) {
+                    Intent intent = new Intent(MainActivity.this,WebActivity.class);
+                    intent.putExtra(WebActivity.URL, "http://demo.mqphp.com/#/slider");
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"请输入网址",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
